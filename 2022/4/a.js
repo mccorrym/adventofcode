59e736926deeb2c913dc9980666f392d5f3f3c7b
mccorrym <mccorrym@gmail.com>
2022-12-05 00:15:25 -0500

    Day 4 results.

---
+++ 2022/4/a.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+let pairs = [];
+let score = 0;
+
+fs.readFile("../data/4.txt", (err, data) => {
+  if (err) throw err;
+
+  pairs = data.toString().split("\n");
+
+  pairs.map((pair) => {
+    let data = pair.split(",");
+    let ranges = [data[0].split("-"), data[1].split("-")];
+
+    if (
+      (parseInt(ranges[0][1]) >= parseInt(ranges[1][1]) &&
+        parseInt(ranges[0][0]) <= parseInt(ranges[1][0])) ||
+      (parseInt(ranges[1][1]) >= parseInt(ranges[0][1]) &&
+        parseInt(ranges[1][0]) <= parseInt(ranges[0][0]))
+    ) {
+      score++;
+    }
+  });
+
+  console.log("# of ranges that overlap: " + score);
+});
