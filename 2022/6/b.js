2601a3b27a7dd8ebc3c363f3e0c1bb707c7de33a
mccorrym <mccorrym@gmail.com>
2022-12-07 02:10:25 -0500

    Day 6 results.

---
+++ 2022/6/b.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+let letters = [];
+let string;
+
+fs.readFile("../data/6.txt", (err, data) => {
+  if (err) throw err;
+
+  letters = [...data.toString()];
+
+  for (let i = 0; i < letters.length; i++) {
+    string = letters.slice(i, i + 14);
+    let unique = [...new Set(string)];
+    if (unique.length == 14) {
+      console.log("First unique set of 4 is at index", i + 14, unique);
+      break;
+    }
+  }
+});
